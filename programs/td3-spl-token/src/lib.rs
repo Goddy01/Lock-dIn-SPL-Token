247b503d444ba5864643b8950678357ac124ab2d
Goddy01 <adigungodwin2@gmail.com>
2024-12-03 03:55:16 +0100

    first commit

---
+++ programs/td3-spl-token/src/lib.rs
@@ -0,0 +1,13 @@
+use anchor_lang::prelude::*;
+
+declare_id!("6QFJhtyFrtYHVFbzDat9k6UshybzKih5d8rksRvbvQea");
+
+#[program]
+pub mod td3_spl_token {
+    use super::*;
+
+    
+}
+
+#[derive(Accounts)]
+
